5b7405f60a760caa4fd68f782c4ca41c64994c1d
Sabastine <sabastinechika123@gmail.com>
2025-06-24 02:47:33 +0100

    commented render migration code

--- app/routers/migration.py
+++ app/routers/migration.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter
-import subprocess
+# uncomment this code to enable migration endpoint 
+# using render and trying to minimize cost that comes with proper migration
 
-router = APIRouter(
-    prefix="/migration",
-    tags=["migration"]
-)
-@router.get("/run-migrations")
-def run_migrations():
-    subprocess.run(["alembic", "upgrade", "head"])
-    return {"message": "Migrations applied"}
+
+# from fastapi import APIRouter
+# import subprocess
+
+# router = APIRouter(
+#     prefix="/migration",
+#     tags=["migration"]
+# )
+# @router.get("/run-migrations")
+# def run_migrations():
+#     subprocess.run(["alembic", "upgrade", "head"])
+#     return {"message": "Migrations applied"}
